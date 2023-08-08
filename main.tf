c57a5c30500e4b422113182d44aeea6abf9ec0b9
Khyathi304 <khyathikamireddy04@gmail.com>
2023-08-08 17:13:30 +0100

    Terraform

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ module "components" {
 
   for_each              = var.component
 
-  source                = "./module"
   zone_id               = var.zone_id
   security_groups       = var.security_groups
   name                  = each.value["name"]
